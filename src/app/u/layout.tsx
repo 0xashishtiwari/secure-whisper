b8c204a6115cd1fc1320c411aceff00dab5450a1
0xashishtiwari <ashishinrewa@gmail.com>
2025-12-28 13:29:18 +0530

    Added speed insight

--- src/app/u/layout.tsx
+++ src/app/u/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "@/components/ui/Navbar";
 
